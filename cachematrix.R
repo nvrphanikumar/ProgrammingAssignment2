6016d146a427b2400b86e65218cab9a62bceaef3
nvrphanikumar <nvrphanikumar@gmail.com>
2015-12-27 15:10:46 -0500

    Code Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##makeCache creates a function that stores values while cacheSolve is used to check if a value 
+# have already been calculated for a given matrix.
 
 ## Write a short comment describing this function
-
+##makeCacheMatrix function creates an environment thats stores the functions.
+#We will later subset the functions within it to retrieve stored results or Recalculate by making a
+##new matrix definitions
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   getinput <- function() x
@@ -15,10 +19,11 @@ makeCacheMatrix <- function(x = matrix()) {
   getinv <- function() m
  list(getinput = getinput ,setinput = setinput ,setinv = setinv, getinv = getinv)
 }
-##Phani
+
 
 ## Write a short comment describing this function
-
+# checks to see if a inverse of the given matrix is already available in cache.
+#if it doesnt,its recalculated
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
